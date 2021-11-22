88d7ec6cdcab4e93736019d55d800470cb279899
xl9p <31505793+xl9p@users.noreply.github.com>
2021-11-22 21:32:34 +0100

    fix atempt ver 2

--- lab 2. Analog therm/ver2/main.py
+++ lab 2. Analog therm/ver2/main.py
@@ -8,18 +8,17 @@ AMB_TEMP_K = 298.15
 R1 = 100
 VREF = 3.3
 RES_AT_AMB_TEMP = 103
-MAX_DC_VALUE = 4096
+MAX_ADC_VALUE = 1023
 
 
 def ThermistorRes(raw_adc):
-    mVol = (raw_adc / MAX_DC_VALUE) * VREF
-    return ((R1 * VREF) / mVol) - R1
+    return R1 * ( (MAX_ADC_VALUE / raw_adc) - 1)
 
 def CalculateTemp(t_res):
     temp_k = (BETA * AMB_TEMP_K) / (BETA + (AMB_TEMP_K * math.log(t_res / RES_AT_AMB_TEMP)))
     return temp_k - 273.15
 
-adc = machine.ADC(bits=12)
+adc = machine.ADC(bits=10)
 apin = adc.channel(pin='P16', attn=adc.ATTN_0DB)
 
 while True:
