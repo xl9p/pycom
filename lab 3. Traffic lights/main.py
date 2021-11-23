4eba75b8c242e29ab37847a1785cea763d3a17e4
xl9p <31505793+xl9p@users.noreply.github.com>
2021-11-23 20:49:11 +0100

    Update main.py

--- lab 3. Traffic lights/main.py
+++ lab 3. Traffic lights/main.py
@@ -77,8 +77,8 @@ greenLED_t = Pin("P8", mode=Pin.OUT) #Make GPIO P10 an output
 
 yellowLED_b = Pin("P7", mode=Pin.OUT)
 
-redLED_p = Pin("P20", mode=Pin.OUT)
-greenLED_p = Pin("P19", mode=Pin.OUT)
+redLED_p = Pin("P21", mode=Pin.OUT)
+greenLED_p = Pin("P20", mode=Pin.OUT)
 
 buttonPin = Pin("P11", mode=Pin.IN, pull=None) # Make GPIO P11 an input
 buzzer = Pin("P6") # Make GPIO P6 an output
