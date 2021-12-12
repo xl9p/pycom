ed33dd48d24ab778011bd29437578afedffd2ec8
xl9p <31505793+xl9p@users.noreply.github.com>
2021-12-12 19:15:37 +0100

    Update main.py

--- lab 2. Digitalt therm - Copy/main.py
+++ lab 2. Digitalt therm - Copy/main.py
@@ -1,58 +1,58 @@
-import time
-from dth import DTH
 from network import LoRa
-import ubinascii
 import socket
+import time
+import ubinascii
 
-
-
-
-app_eui = ubinascii.unhexlify('70B3D549779D7B99')
-app_key = ubinascii.unhexlify('99DD8A6AE643C8A591A709672FE35C85')
-
+# Initialise LoRa in LORAWAN mode.
+# Please pick the region that matches where you are using the device:
+# Asia = LoRa.AS923
+# Australia = LoRa.AU915
+# Europe = LoRa.EU868
+# United States = LoRa.US915
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
-th = DTH('P11', 0)
 
-while True:
-    try:
-        if not lora.has_joined():
-            
-        else:
-            print("\nDigital sensor's data:")
-            result = th.read()
-            if result.is_valid():
-                s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
-                s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
-                s.setblocking(True)
-                s.send(bytes([0x01, 0x02, 0x03]))
-                s.setblocking(False)
-                data = s.recv(64)
-                print(data)
+# create an OTAA authentication parameters, change them to the provided credentials
+app_eui = ubinascii.unhexlify('0000000000000000')
+app_key = ubinascii.unhexlify('40E004E1CD3531F4DEC22622E110CF1B')
+#uncomment to use LoRaWAN application provided dev_eui
+#dev_eui = ubinascii.unhexlify('70B3D549938EA1EE')
 
-                print("Results are valid.")
-                print("Temperature: " + str(result.temperature)+"C")
-                print("Humidity: " + str(result.humidity)+"%")
-            else:
-                print("Results may be not valid.")
-                print("Temperature: " + str(result.temperature)+"C")
-                print("Humidity: " + str(result.humidity)+"%")
-    except Exception as err:
-        print(str(err))
-    time.sleep(3)
+# Uncomment for US915 / AU915 & Pygate
+# for i in range(0,8):
+#     lora.remove_channel(i)
+# for i in range(16,65):
+#     lora.remove_channel(i)
+# for i in range(66,72):
+#     lora.remove_channel(i)
 
-#end device id eui-70b3d57ed0049a63
-# joineui 0000000000000000
-#DEVeui 70B3D57ED0049A63
-#App key 8F74330E23A64634D5F7F07F1234006B
+# join a network using OTAA (Over the Air Activation)
+#uncomment below to use LoRaWAN application provided dev_eui
+lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
+#lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
+# wait until the module has joined the network
+while not lora.has_joined():
+    time.sleep(2.5)
+    print('Not yet joined...')
 
-
+print('Joined')
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
 # set the LoRaWAN data rate
-
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
 
 # make the socket blocking
 # (waits for the data to be sent and for the 2 receive windows to expire)
-
+s.setblocking(True)
 
 # send some data
+s.send(bytes([0x01, 0x02, 0x03]))
+
+# make the socket non-blocking
+# (because if there's no data received it will block forever...)
+s.setblocking(False)
+
+# get any data received (if any...)
+data = s.recv(64)
+print(data)
