42045f672e051c8c39f973d58354396b27e54a81
xl9p <31505793+xl9p@users.noreply.github.com>
2021-12-13 15:18:40 -0800

    Update main.py

--- lab 2. Digitalt therm - Copy/main.py
+++ lab 2. Digitalt therm - Copy/main.py
@@ -2,57 +2,55 @@ from network import LoRa
 import socket
 import time
 import ubinascii
+import ustruct
+import time
+from machine import Pin
+from dth import DTH
 
-# Initialise LoRa in LORAWAN mode.
-# Please pick the region that matches where you are using the device:
-# Asia = LoRa.AS923
-# Australia = LoRa.AU915
-# Europe = LoRa.EU868
-# United States = LoRa.US915
+
+def encode_float(temp):
+    packet = ustruct.pack('f', temp)
+
+    s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+    s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
+
+    s.setblocking(True)
+
+    s.send(packet)
+
+    s.setblocking(False)
+    
+    data = s.recv(64)
+    print(data)
+    print("Unpacked value is:", ustruct.unpack('f', packet)[0])
+    print("data sent")
+
 lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU868)
 
-# create an OTAA authentication parameters, change them to the provided credentials
 app_eui = ubinascii.unhexlify('0000000000000000')
 app_key = ubinascii.unhexlify('40E004E1CD3531F4DEC22622E110CF1B')
-#uncomment to use LoRaWAN application provided dev_eui
 dev_eui = ubinascii.unhexlify('70B3D57ED0049D35')
 
-# Uncomment for US915 / AU915 & Pygate
-# for i in range(0,8):
-#     lora.remove_channel(i)
-# for i in range(16,65):
-#     lora.remove_channel(i)
-# for i in range(66,72):
-#     lora.remove_channel(i)
-
-# join a network using OTAA (Over the Air Activation)
-#uncomment below to use LoRaWAN application provided dev_eui
-#lora.join(activation=LoRa.OTAA, auth=(app_eui, app_key), timeout=0)
 lora.join(activation=LoRa.OTAA, auth=(dev_eui, app_eui, app_key), timeout=0)
 
-# wait until the module has joined the network
 while not lora.has_joined():
     time.sleep(2.5)
     print('Not yet joined...')
 
 print('Joined')
-# create a LoRa socket
-s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
 
-# set the LoRaWAN data rate
-s.setsockopt(socket.SOL_LORA, socket.SO_DR, 0)
+th = DTH('P11', 0)
 
-# make the socket blocking
-# (waits for the data to be sent and for the 2 receive windows to expire)
-s.setblocking(True)
+while True:
+    print("\nDigital sensor's data:")
+    result = th.read()
+    if result.is_valid():
+        temp = float(result.temperature)
+        humid = float(result.humidity)
+        print("Results are valid.")
+        print("Temperature: " + str(temp)+"C")
+        print("Humidity: " + str(humid)+"%")
+        encode_float(temp)
 
-# send some data
-s.send(bytes([0x01, 0x02, 0x03]))
-
-# make the socket non-blocking
-# (because if there's no data received it will block forever...)
-s.setblocking(False)
-
-# get any data received (if any...)
-data = s.recv(64)
-print(data)
+    time.sleep(65)
