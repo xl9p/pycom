a092a6d137fab627d456a9542411e643808c7177
Ivan Shtuka <besheinuybanan25@gmail.com>
2021-12-14 01:51:21 +0100

    Update main.py

--- supermega temp/main.py
+++ supermega temp/main.py
@@ -1,7 +1,8 @@
 from machine import Pin, I2C
 from bmp085 import BMP180
 
-i2c = I2C(0, I2C.MASTER, baudrate=20000)
+i2c = I2C(0, pins=('P10','P11'))
+i2c.init(I2C.MASTER, baudrate=20000)
 bmp = BMP180(i2c)
 bmp.oversample = 2
 bmp.sealevel = 101325
