db66dffc8a671a3453664356e4ee701e326a78f8
xl9p <31505793+xl9p@users.noreply.github.com>
2021-12-13 17:44:30 -0800

    sdds

--- supermega temp/main.py
+++ supermega temp/main.py
@@ -1,13 +1,15 @@
 from machine import Pin, I2C
 from bmp085 import BMP180
 
-i2c = I2C(0, pins=('P10','P11'))
-i2c.init(I2C.MASTER, baudrate=20000)
-bmp = BMP180(i2c)
-bmp.oversample = 2
-bmp.sealevel = 101325
 
-temp = bmp.temperature
-p = bmp.pressure
-altitude = bmp.altitude
-print(temp, p, altitude)
+print("test")
+i2c = I2C()
+bmp = BMP180(i2c)
+bmp.oversample = 3
+bmp.sealevel = 1013.25
+
+while True:
+    temp = bmp.temperature
+    p = bmp.pressure
+    altitude = bmp.altitude
+    print(temp, p, altitude)
